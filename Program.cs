bc7d1698c9fd6d90bff4e8aabb793de346472e92
Cornel <cornelhuman@gmail.com>
2021-07-10 12:02:20 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+
+namespace QuickBlueToothLE
+{
+    class Program
+    {
+        static DeviceInformation device = null;
+
+        public static string HEART_RATE_SERVICE_ID = "180d";
+
+        static async Task Main(string[] args)
+        {
+            // Query for extra properties you want returned
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            DeviceWatcher deviceWatcher =
+                        DeviceInformation.CreateWatcher(
+                                BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                requestedProperties,
+                                DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+
+            // EnumerationCompleted and Stopped are optional to implement.
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            // Start the watcher.
+            deviceWatcher.Start();
+            while (true)
+            {
+                if (device == null)
+                {
+                    Thread.Sleep(200);
+                }
+                else
+                {
+                    Console.WriteLine("Press Any to pair with Wahoo Ticker");
+                    Console.ReadKey();
+                    BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(device.Id);
+                    Console.WriteLine("Attempting to pair with device");
+                    GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+
+                    if (result.Status == GattCommunicationStatus.Success)
+                    {
+                        Console.WriteLine("Pairing succeeded");
+                        var services = result.Services;
+                        foreach (var service in services)
+                        {
+                            if (service.Uuid.ToString("N").Substring(4, 4) == HEART_RATE_SERVICE_ID)
+                            {
+                                Console.WriteLine("Found heart rate service");
+                                GattCharacteristicsResult charactiristicResult = await service.GetCharacteristicsAsync();
+
+                                if (charactiristicResult.Status == GattCommunicationStatus.Success)
+                                {
+                                    var characteristics = charactiristicResult.Characteristics;
+                                    foreach (var characteristic in characteristics)
+                                    {
+                                        Console.WriteLine("---------------");
+                                        Console.WriteLine(characteristic);
+                                        GattCharacteristicProperties properties = characteristic.CharacteristicProperties;
+
+                                        if (properties.HasFlag(GattCharacteristicProperties.Notify))
+                                        {
+                                            Console.WriteLine("Notify poroperty found");
+                                            GattCommunicationStatus status = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                        GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                                            if (status == GattCommunicationStatus.Success)
+                                            {
+                                                characteristic.ValueChanged += Characteristic_ValueChanged;
+                                                // Server has been informed of clients interest.
+                                            }
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Press Any Key to Exit application");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+            deviceWatcher.Stop();
+        }
+
+        private static void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var reader = DataReader.FromBuffer(args.CharacteristicValue);
+            var flags = reader.ReadByte();
+            var value = reader.ReadByte();
+            Console.WriteLine($"{flags} - {value}");
+        }
+
+        private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            Console.WriteLine(args.Name);
+            if (args.Name == "TICKR 2966")
+                device = args;
+            //throw new NotImplementedException();
+        }
+    }
+}
