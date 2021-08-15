4a0e4f34452e77ac662e1aae14b1c40c3a05c8de
Cornel <cornelhuman@gmail.com>
2021-08-15 19:37:13 +0100

    Set Resistance on Kickr Core

--- Program.cs
+++ Program.cs
@@ -15,7 +15,8 @@ namespace QuickBlueToothLE
     {
         static DeviceInformation device = null;
 
-        public static string HEART_RATE_SERVICE_ID = "180d";
+        public static string HEART_RATE_SERVICE_ID = "180D";
+        public static string FITNESSMACHINE = "1826";
 
         static async Task Main(string[] args)
         {
@@ -48,7 +49,7 @@ namespace QuickBlueToothLE
                 }
                 else
                 {
-                    Console.WriteLine("Press Any to pair with Wahoo Ticker");
+                    Console.WriteLine("Press Any to pair with device");
                     Console.ReadKey();
                     BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(device.Id);
                     Console.WriteLine("Attempting to pair with device");
@@ -60,35 +61,104 @@ namespace QuickBlueToothLE
                         var services = result.Services;
                         foreach (var service in services)
                         {
-                            if (service.Uuid.ToString("N").Substring(4, 4) == HEART_RATE_SERVICE_ID)
+                            Console.WriteLine($"{service.Uuid}");
+                            Console.WriteLine("---------------");
+                            GattCharacteristicsResult charactiristicResult = await service.GetCharacteristicsAsync();
+
+                            if (service.Uuid.ToString("N").Substring(4, 4).ToUpper() == FITNESSMACHINE)
                             {
-                                Console.WriteLine("Found heart rate service");
-                                GattCharacteristicsResult charactiristicResult = await service.GetCharacteristicsAsync();
 
                                 if (charactiristicResult.Status == GattCommunicationStatus.Success)
                                 {
                                     var characteristics = charactiristicResult.Characteristics;
                                     foreach (var characteristic in characteristics)
                                     {
-                                        Console.WriteLine("---------------");
-                                        Console.WriteLine(characteristic);
+                                        var characteristicKey = characteristic.Uuid.ToString("N").Substring(4, 4).ToUpper();
+                                        string characteristicName = "Unknown";
+                                        if (GattServiceUUID.Lookup.ContainsKey(characteristicKey))
+                                            characteristicName = GattServiceUUID.Lookup[characteristicKey].Item2;
+
+
+                                        Console.WriteLine($"\t [{characteristicName}] - [{characteristic.Uuid}]");
+
                                         GattCharacteristicProperties properties = characteristic.CharacteristicProperties;
 
-                                        if (properties.HasFlag(GattCharacteristicProperties.Notify))
+                                        if (properties.HasFlag(GattCharacteristicProperties.Read))
                                         {
-                                            Console.WriteLine("Notify poroperty found");
+                                            if(characteristicName == "Supported Resistance Level Range")
+                                            {
+                                                var supportedResitanceLevelRange = await characteristic.ReadValueAsync();
+                                                if (supportedResitanceLevelRange.Status == GattCommunicationStatus.Success)
+                                                {
+                                                    var reader = DataReader.FromBuffer(supportedResitanceLevelRange.Value);
+                                                    byte[] input = new byte[reader.UnconsumedBufferLength];
+                                                    reader.ReadBytes(input);
+                                                    // Utilize the data as needed
+                                                }
+                                            }
+
+                                            Console.WriteLine("\t\t [{characteristic.Uuid}] Read property found");
+
+                                        }
+                                        else if (properties.HasFlag(GattCharacteristicProperties.Notify))
+                                        {
+                                            Console.WriteLine("\t\t [{characteristic.Uuid}] Notify property found");
                                             GattCommunicationStatus status = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
-                        GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                                            GattClientCharacteristicConfigurationDescriptorValue.Notify);
                                             if (status == GattCommunicationStatus.Success)
                                             {
                                                 characteristic.ValueChanged += Characteristic_ValueChanged;
                                                 // Server has been informed of clients interest.
                                             }
                                         }
-                                        
+                                        else if (properties.HasFlag(GattCharacteristicProperties.Write))
+                                        {
+                                            Console.WriteLine($"\t\t [{characteristic.Uuid}] Write property found");
+                                            var writer = new DataWriter();
+                                            // WriteByte used for simplicity. Other common functions - WriteInt16 and WriteSingle
+                                            writer.WriteByte(0x00);
+
+                                            GattCommunicationStatus resultWrite = await characteristic.WriteValueAsync(writer.DetachBuffer());
+                                            if (resultWrite == GattCommunicationStatus.Success)
+                                            {
+                                                Console.WriteLine("Control request success");
+                                            }
+
+                                            writer = new DataWriter();
+                                            // WriteByte used for simplicity. Other common functions - WriteInt16 and WriteSingle
+                                            writer.WriteByte(0x01);
+
+                                            resultWrite = await characteristic.WriteValueAsync(writer.DetachBuffer());
+                                            if (resultWrite == GattCommunicationStatus.Success)
+                                            {
+                                                Console.WriteLine("Reset request success");
+                                            }
+
+                                            writer = new DataWriter();
+                                            // WriteByte used for simplicity. Other common functions - WriteInt16 and WriteSingle
+                                            writer.WriteByte(0x04);
+                                            writer.WriteByte(0);
+
+                                            resultWrite = await characteristic.WriteValueAsync(writer.DetachBuffer());
+                                            if (resultWrite == GattCommunicationStatus.Success)
+                                            {
+                                                Console.WriteLine("Resistance request success");
+                                            }
+
+
+
+
+                                        }
+                                        else if (properties.HasFlag(GattCharacteristicProperties.WriteWithoutResponse))
+                                        {
+                                            Console.WriteLine($"\t\t [{characteristic.Uuid}] WriteWithoutResponse property found");
+                                        }
+
+
                                     }
                                 }
                             }
+
                         }
                     }
 
@@ -102,10 +172,12 @@ namespace QuickBlueToothLE
 
         private static void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
         {
+            /*
             var reader = DataReader.FromBuffer(args.CharacteristicValue);
             var flags = reader.ReadByte();
             var value = reader.ReadByte();
             Console.WriteLine($"{flags} - {value}");
+            */
         }
 
         private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
@@ -130,8 +202,11 @@ namespace QuickBlueToothLE
 
         private static void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
         {
+
             Console.WriteLine(args.Name);
             if (args.Name == "TICKR 2966")
+                device = args;
+            else if (args.Name == "KICKR CORE 802B")
                 device = args;
             //throw new NotImplementedException();
         }
